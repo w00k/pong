f20042290a5646d43bd7e393ade13e999afdc5fd
w00k <wook82@gmail.com>
2021-04-27 14:36:59 -0400

    se sube servicio pong con README.md y comentarios en el src

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# Services pong
-Pong is a microservice, this will be deployed on kubernetes ...
+# Services Pong
+Servicio **pong** es un servicio que será deployado en kubernetes. 
 
+## Pre-requisitos
 
+Tener instalado: 
+* Golang. 
+* Descargar las dependencias. 
+
+En la ruta donde está el proyecto 
+```bash 
+go mod download
+```
+
+## Ejecutar
+
+Solo para ejecutar 
+```bash 
+go run pong.go
+```
+
+Para compilar en Windows
+```bash 
+go build -o path/pong.exe pong.go
+```
--- pong.go
+++ pong.go
@@ -13,6 +13,7 @@ type App struct {
 	Router *mux.Router
 }
 
+// pongHandler ::: controlador del servicio pong
 func pongHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("pongHandler - request")
 	var response map[string]interface{}
@@ -20,22 +21,26 @@ func pongHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, response)
 }
 
+// respondWithJSON :: agrega la cabecera de JSON a la respuesta
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(payload)
 }
 
+// initialiseRoutes ::: contiene el controlador y path de servicio ping
 func (app *App) initialiseRoutes() {
 	log.Println("start - pong service up")
 	app.Router = mux.NewRouter()
 	app.Router.HandleFunc("/pong", pongHandler)
 }
 
+// run ::: levanta el servidor en puerto indicado
 func (app *App) run() {
 	log.Fatal(http.ListenAndServe(":8081", app.Router))
 }
 
+// main ::: inicio del servicio ping
 func main() {
 	app := App{}
 	app.initialiseRoutes()
